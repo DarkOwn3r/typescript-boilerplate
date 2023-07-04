4bb91aaa7dba6852a1888cb0a9de48b53fdb838a
Pablo Santana <pablosntojd@gmail.com>
2023-07-04 19:26:50 +0100

    feat: add logic to return list powered by two from various numbers

--- __test__/power-of-two.spec.ts
+++ __test__/power-of-two.spec.ts
@@ -6,3 +6,9 @@ describe("powerOfTwo", () => {
     expect(getPowerOfTwo(numbers)).toEqual([4])
     });
 })
+describe("powerOfTwo", () => {
+    it("should return a list of powered by two numbers from a list with one number", () => {
+        const numbers: Array<number> = [2, 4];
+    expect(getPowerOfTwo(numbers)).toEqual([4, 16])
+    });
+})
