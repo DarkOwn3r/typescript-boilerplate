4adc2a3e76c3f4cb09f1601c364782195c716972
Pablo Santana <pablosntojd@gmail.com>
2023-07-04 19:22:08 +0100

    fix: change expected solution of tests and function

--- __test__/power-of-two.spec.ts
+++ __test__/power-of-two.spec.ts
@@ -1,8 +1,8 @@
 import { getPowerOfTwo } from "../src/power-of-two"
 
 describe("powerOfTwo", () => {
-    it("should return a list of power of two numbers from a list with one number", () => {
+    it("should return a list of powered by two numbers from a list with one number", () => {
         const numbers: Array<number> = [2];
-    expect(getPowerOfTwo(numbers)).toEqual([2])
+    expect(getPowerOfTwo(numbers)).toEqual([4])
     });
 })
